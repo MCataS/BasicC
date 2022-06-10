9be62e4179ccc04fe9c838a7ff9b6feb19e38b93
Margie Sarmiento <59615472+MCataS@users.noreply.github.com>
2022-06-10 11:07:50 -0500

    Creation

---
+++ IfElse/IfElse/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 85;
+            if (num < 50)
+            {
+                Console.WriteLine("incorrecto");
+            }
+            else
+            {
+                Console.WriteLine("Correcto Cata, te quedo bien");
+            }
+        }
+    }
+}
---
+++ PrimeraPrueba/PrimeraPrueba/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PrimeraPrueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre ;
+            Console.WriteLine("Cuál es tu nombre?");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+        }
+    }
+}
